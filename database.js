c3d8b8f84f09fa8ead760dc09258bd3d40107782
SabaHilal <57343679+SabaHilal@users.noreply.github.com>
2021-01-09 12:31:26 +0530

    Add files via upload

---
+++ database.js
@@ -0,0 +1,11 @@
+const dictionary = {
+    "door" : {
+    "word" : "door",
+    "inSanskrit": "dura-",
+    "SanskritScript":"दुर्",
+    "definition" : "gate, door, entrance"
+  },
+  
+}
+
+export default dictionary
